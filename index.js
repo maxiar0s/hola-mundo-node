0c0356b821699ab48efe74343a06245254bbf4ff
maxiar0s <maxi.aros.osorio@gmail.com>
2024-10-16 11:34:55 -0300

    fix

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Note = require("./models/note");
 let notes = [];
 
 //Middlewares
+app.use(express.static("dist"));
+
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
@@ -15,17 +17,16 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
-app.use(express.static("dist"));
-app.use(cors());
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.get("/", (request, response) => {
-  response.send(notes);
+  response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/notes", (request, response) => {
@@ -34,17 +35,17 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
+// app.get("/api/notes/:id", (request, response, next) => {
+//   Note.findById(request.params.id)
+//     .then((note) => {
+//       if (note) {
+//         response.json(note);
+//       } else {
+//         response.status(404).end();
+//       }
+//     })
+//     .catch((error) => next(error));
+// });
 
 // const generateId = () => {
 //   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
@@ -84,20 +85,27 @@ app.put("/api/notes/:id", (request, response, next) => {
 //   // }
 // });
 
-app.post("/api/notes", (request, response, next) => {
+app.post("/api/notes", (request, response) => {
   const body = request.body;
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
