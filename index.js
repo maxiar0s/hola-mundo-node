afb71a9209b327838a8bf38de2c5e4b809ddfbe2
maxiar0s <maxi.aros.osorio@gmail.com>
2024-10-16 10:29:33 -0300

    .env

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Note = require("./models/note");
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+let notes = [];
 
+//Middlewares
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
@@ -42,57 +29,56 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  // const maxIdString = maxId.toString();
-  return (maxId + 1).toString();
-};
+// const generateId = () => {
+//   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+//   // const maxIdString = maxId.toString();
+//   return (maxId + 1).toString();
+// };
 
-app.put("/api/notes/:id", (request, response) => {
+app.put("/api/notes/:id", (request, response, next) => {
+  // app.put("/api/notes/:id", (request, response) => {
   const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-
-  if (!note) {
-    response.status(404).end();
-  } else {
-    const updatedNote = { ...note, ...request.body };
-    notes = notes.map((n) => (n.id === id ? updatedNote : n));
-    response.json(updatedNote);
-  }
+  // const note = notes.find((note) => note.id === id);
+  Note.findByIdAndUpdate(id, request.body, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+  // if (!note) {
+  //   response.status(404).end();
+  // } else {
+  //   const updatedNote = { ...note, ...request.body };
+  //   notes = notes.map((n) => (n.id === id ? updatedNote : n));
+  //   response.json(updatedNote);
+  // }
 });
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    console.log("x");
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -104,7 +90,7 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://admin:${password}@cluster0.nwezd.mongodb.net/notesApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url).then(() => {
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+  });
+
+  const Note = mongoose.model("Note", noteSchema);
+
+  const note = new Note({
+    content: process.argv[3],
+    important: process.argv[4] === "true" ? true : false,
+  });
+
+  if (process.argv.length === 3) {
+    Note.find({}).then((result) => {
+      result.forEach((note) => {
+        console.log(note);
+      });
+      mongoose.connection.close();
+    });
+  } else if (process.argv.length === 5) {
+    note.save().then((result) => {
+      console.log("note saved!");
+      mongoose.connection.close();
+    });
+  }
+});
